bfb0d9a170e00172d43b442317c9e7c01f016a3f
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2022-08-20 23:44:22 -0300

    adds functionality of finding matching cards not being able to flip them

--- js/index.js
+++ js/index.js
@@ -1,31 +1,77 @@
-const inputUser = 14
+import { dealWithImages } from './deal-with-images.js'
+import { CardComponent } from './components/card.js'
 
+const inputUser = 6
+
+const Card = (id, img, isOpen = false) => ({
+    id, isOpen, img
+})
 
 const MainComponent = () => {
     const main = document.querySelector('main')
 
-    const CardComponent = () => {
-        const card = document.createElement('arcticle')
-        card.classList.add('card')
+    const gernerateCardList = (length) => {
+        const image = dealWithImages(length)
+        const randomImagePaths = image.getRandomImagePaths()
+        const cardList = []
 
-        const imageFront = document.createElement('img')
-        imageFront.setAttribute('src', './assets/front.png')
+        randomImagePaths.forEach((imagePath, index) => {
+            const id = index + 1
+            const img = imagePath
+            const card = Card(id, img)
+            cardList.push(card)
+        })
 
-        card.appendChild(imageFront)
-
-        return card
+        return cardList
     }
 
-    for (let i = 0; i < inputUser; i++) {
-        main.appendChild(CardComponent())
+    const renderCardComponentList = (length) => {
+        const cardList = gernerateCardList(length)
+
+        const toggle = (card, cardComponent) => {
+            if (!card.isOpen) {
+                cardComponent.classList.toggle('is-flipped', true);
+                card.isOpen = true
+            } else {
+                const otherCards = cardList.filter(s => s.id !== card.id)
+                const foundCard = otherCards.find(c =>
+                    c.img === card.img && c.isOpen === card.isOpen)
+
+                if (foundCard) {
+                    card.isOpen = true
+                } else {
+                    cardComponent.classList.toggle('is-flipped', false);
+                    card.isOpen = false
+                }
+            }
+        }
+
+        const cardComponentList = cardList.map(card => {
+            const cardComponent = CardComponent({
+                faceBackImg: card.img,
+                onclick: () => {
+                    toggle(card, cardComponent)
+                }
+            })
+            return cardComponent
+        })
+
+        return cardComponentList
     }
 
+    const render = () => {
+        const main = document.querySelector('main')
+        const cardList = renderCardComponentList(inputUser)
+
+        cardList.forEach(cardComponent => {
+            main.appendChild(cardComponent)
+        })
+
+    }
+
+    render()
 }
 
 (function App() {
     MainComponent()
-
 })()
-
-
-
