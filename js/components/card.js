f19d62ab81cb24db365059effc07468a1530a222
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2022-08-20 23:42:48 -0300

    refactor: isolates the logic that deals with the card

---
+++ js/components/card.js
@@ -0,0 +1,37 @@
+export const CardComponent = ({ faceBackImg, onclick }) => {
+
+    const render = () => {
+        const card = document.createElement('arcticle')
+        card.classList.add('card-wrapper')
+        card.onclick = onclick
+        card.appendChild(renderFace())
+        card.appendChild(renderBack())
+
+        return card
+    }
+
+    const renderFace = () => {
+        const div = document.createElement('div')
+        div.classList.add('card-face', 'face-front')
+        const img = renderImg()
+        div.appendChild(img)
+        return div
+    }
+
+    const renderBack = () => {
+        const div = document.createElement('div')
+        div.classList.add('card-face', 'face-back')
+        const img = renderImg(faceBackImg)
+        div.appendChild(img)
+        return div
+    }
+
+    const renderImg = (path = './assets/front.png') => {
+        const imageFront = document.createElement('img')
+        imageFront.setAttribute('src', path)
+        return imageFront
+    }
+
+
+    return render()
+}
