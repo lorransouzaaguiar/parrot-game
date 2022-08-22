25636eb9f1a277ed0e426148146f81ba784a27b7
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2022-08-22 17:22:39 -0300

    refactor: remove code complexity

--- js/components/main.js
+++ js/components/main.js
@@ -1,31 +1,16 @@
 import { dealWithImages } from '../deal-with-images.js'
-import { Card } from '../model/card.js'
 import { CardComponent } from './card.js'
 
 export const MainComponent = () => {
 
-    const gernerateCardList = (length) => {
+    const renderCardComponentList = (length) => {
         const image = dealWithImages(length)
         const randomImagePaths = image.getRandomImagePaths()
-        const cardList = []
 
-        randomImagePaths.forEach((imagePath, index) => {
-            const id = index + 1
-            const img = imagePath
-            const card = Card(id, img)
-            cardList.push(card)
-        })
-
-        return cardList
-    }
-
-    const renderCardComponentList = (length) => {
-        const cardList = gernerateCardList(length)
-
-        const cardComponentList = cardList.map((card) => {
+        const cardComponentList = randomImagePaths.map((img, index) => {
             const cardComponent = CardComponent({
-                key: card.id,
-                faceBackImg: card.img
+                key: index + 1,
+                faceBackImg: img
             })
             return cardComponent
         })
